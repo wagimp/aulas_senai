7113093a10fd40bb76338fd48a076f3f323f5670
Wagner R. da Silva <wagaum@gmail.com>
2026-04-27 16:15:51 -0300

    calcular nota

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,13 @@
-print("Olá GitHub!")
+def calcular_media(nota1, nota2, nota3):
+    soma = nota1 + nota2 + nota3
+    media = soma / 3
+    return media
+# Programa principal (Fora da função)
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a primeira nota: "))
+n3 = float(input("Digite a primeira nota: "))
+
+resultado = calcular_media(n1, n2, n3)
+
+print("Média final:", resultado)
+
