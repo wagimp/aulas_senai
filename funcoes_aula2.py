3e60e8f97f7ae1aeeac24794b76d625a5c0046b7
Wagner Rodrigues da Silva <wagaum@gmail.com>
2026-04-28 14:23:05 -0300

    funções

--- funcoes_aula2.py
+++ funcoes_aula2.py
@@ -1,1 +1,11 @@
+def formatar_real_replace(valor):
+    texto = f"R$ {valor:,.2f}"   # padrão EUA: 1,234.56
+    texto = texto.replace(",", "X")
+    texto = texto.replace(".", ",")
+    texto = texto.replace("X", ".")
+    return texto
 
+
+# Uso
+preco = 1234.5
+print(formatar_real_replace(preco))  # R$ 1.234,50
